d9ddeed28e604a81c593e3c5355d8966d4fa4af8
Reiker Seiffe <reikerseiffe@gmail.com>
2019-03-26 00:20:02 -0500

    Filled out enums

--- Eve Tech II Production Calculator/Models/Enums/ProcessedMoonMaterial.swift
+++ Eve Tech II Production Calculator/Models/Enums/ProcessedMoonMaterial.swift
@@ -9,6 +9,24 @@
 import Foundation
 
 enum ProcessedMoonMaterial:String, Codable{
-    case titaniumChromide = "Titanium Chromide"
+    case caesariumCadmide = "Caesarium Cadmide"
+    case carbonPolymers = "Carbon Polymers"
+    case ceramicPowder = "Ceramic Powder"
+    case crystalliteAlloy = "Crystallite Alloy"
+    case dysporite = "Dysporite"
+    case ferniteAlloy = "Fernite Alloy"
+    case ferrofluid = "Ferrofluid"
+    case fluxedCondensates = "Fluxed Condensates"
+    case hexite = "Hexite"
+    case hyperflurite = "Hyperflurite"
+    case neoMercurite = "Neo Mercurite"
+    case platinumTechnite = "Platinum Technite"
+    case promethiumMercurite = "Promethium Mercurite"
+    case prometium = "Prometium"
+    case rolledTungstenalloy = "Rolled Tungsten Alloy"
     case siliconDiborite = "Silicon Diborite"
+    case solerium = "Solerium"
+    case sulfuricAcid = "Sulfuric Acid"
+    case thuliumHafnite = "Thulium Hafnite"
+    case titaniumChromide = "Titanium Chromide"
 }
